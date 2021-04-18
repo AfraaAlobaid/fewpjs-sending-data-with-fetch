89a8c34b521c10e740682624b2998dd43315f1f8
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-04-18 17:25:46 +0930

    final deliverable

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(userName, userEmail){
+    const body = {
+        name: userName,
+        email: userEmail
+    }
+    const config = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(body)
+    }
+    return fetch("http://localhost:3000/users", config)
+    .then(resp => resp.json())
+    .then(obj => {
+        document.getElementsByTagName("body")[0].append(obj.id);
+    })
+    .catch((error)=>{
+        document.getElementsByTagName("body")[0].append(error.message);
+    })
+}
